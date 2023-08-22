492f697f0dbfc51b35eb2e155288764a753541c6
Mandytrajano90 <mandytrajano90@gmail.com>
2023-08-22 15:55:51 -0300

    Second Requirement - Form component

---
+++ src/components/Form.tsx
@@ -0,0 +1,30 @@
+function Form() {
+  return (
+    <form className="form-container">
+      <div>
+        <label htmlFor="name">Nome do serviço</label>
+        <input type="text" id="name" placeholder="Nome do serviço" />
+      </div>
+      <div>
+        <label htmlFor="login">Login</label>
+        <input type="text" id="login" placeholder="Login" />
+      </div>
+      <div>
+        <label htmlFor="senha">Senha</label>
+        <input type="password" id="senha" placeholder="Senha" />
+      </div>
+      <div>
+        <label htmlFor="url">URL</label>
+        <input type="text" id="url" placeholder="URL" />
+      </div>
+      <div>
+        <button type="submit" id="sub-button">Cadastrar</button>
+      </div>
+      <div>
+        <button type="submit" id="cancel-button">Cancelar</button>
+      </div>
+    </form>
+  );
+}
+
+export default Form;
