be7b8953d2b52ab2d24afa68b5da8ae3e932b9d4
Mandytrajano90 <mandytrajano90@gmail.com>
2023-08-23 20:08:41 -0300

    Fourth Requirement - Validating inputs

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,28 +1,71 @@
+import { useState } from 'react';
+import { objInput, InputType, verifyInputs } from '../data';
+
 type FormProps = {
   handleClick: () => void;
 };
 
 function Form({ handleClick }: FormProps) {
+  const [inputData, setInputData] = useState<InputType>(objInput);
+
+  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const { name, value } = event.target;
+    console.log(name);
+    setInputData({
+      ...inputData,
+      [name]: value,
+    });
+  }
+
+  const handleSubmit = (event: React.ChangeEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  };
+
   return (
-    <form className="form-container">
+    <form onSubmit={ handleSubmit } className="form-container">
       <div>
         <label htmlFor="name">Nome do serviço</label>
-        <input type="text" id="name" placeholder="Nome do serviço" />
+        <input
+          type="text"
+          name="name"
+          id="name"
+          required
+          placeholder="Nome do serviço"
+          value={ inputData.name }
+          onChange={ handleChange }
+        />
       </div>
       <div>
         <label htmlFor="login">Login</label>
-        <input type="text" id="login" placeholder="Login" />
+        <input
+          type="text"
+          id="login"
+          name="login"
+          required
+          placeholder="Login"
+          value={ inputData.login }
+          onChange={ handleChange }
+        />
       </div>
       <div>
         <label htmlFor="senha">Senha</label>
-        <input type="password" id="senha" placeholder="Senha" />
+        <input
+          type="password"
+          id="senha"
+          name="senha"
+          placeholder="Senha"
+          value={ inputData.senha }
+          onChange={ handleChange }
+        />
       </div>
       <div>
         <label htmlFor="url">URL</label>
         <input type="text" id="url" placeholder="URL" />
       </div>
       <div>
-        <button type="submit" id="sub-button">Cadastrar</button>
+        { verifyInputs(inputData)
+          ? <button disabled={ false }>Cadastrar</button>
+          : <button disabled>Cadastrar</button>}
       </div>
       <div>
         <button onClick={ handleClick } id="cancel-button">Cancelar</button>
---
+++ src/data.ts
@@ -0,0 +1,26 @@
+export type InputType = {
+  name:string,
+  login:string,
+  senha:string,
+  url:string
+};
+
+export const objInput: InputType = {
+  name: '',
+  login: '',
+  senha: '',
+  url: '',
+};
+
+export function verifyInputs(param: InputType): boolean {
+  const { name, login, senha } = param;
+  const regex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[^A-Za-z0-9]).{8,16}$/;
+
+  const verifyName = /^\w/g.test(name);
+  const verifyLogin = /^\w/g.test(login);
+  const verifyPassword = regex.test(senha);
+  const Infos = [verifyName, verifyLogin, verifyPassword];
+  const verifyInfos = Infos
+    .every((info) => info === true);
+  return verifyInfos;
+}
